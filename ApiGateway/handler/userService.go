d2286f3f066a81ea31a16f67163fb8151e20ec9f
deksa99 <deja99@live.com>
2022-08-17 13:50:07 +0200

    API gateway: user service finished

--- ApiGateway/handler/userService.go
+++ ApiGateway/handler/userService.go
@@ -2,7 +2,9 @@ package handler
 
 import (
 	"ApiGateway/util/response"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
@@ -19,19 +21,32 @@ func Register(w http.ResponseWriter, r *http.Request) {
 }
 
 func ChangePassword(w http.ResponseWriter, r *http.Request) {
+	url := response.UserServiceRoundRobin.Next().Host + "/api/users/change-password"
+	// TODO authorize
 
+	handleRequest(w, r, url)
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.ParseUint(params["id"], 10, 32)
 
+	url := response.UserServiceRoundRobin.Next().Host + "/api/users/" + strconv.FormatUint(id, 10)
+	handleRequest(w, r, url)
 }
 
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
-
+	url := response.UserServiceRoundRobin.Next().Host + "/api/users"
+	handleRequest(w, r, url)
 }
 
 func BlockUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, _ := strconv.ParseUint(params["id"], 10, 32)
+	// TODO authorize
 
+	url := response.UserServiceRoundRobin.Next().Host + "/api/users" + strconv.FormatUint(id, 10) + "/block"
+	handleRequest(w, r, url)
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request, url string) {
