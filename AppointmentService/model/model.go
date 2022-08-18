8b0c2172769a5b52754a160cd9567c52af530d0c
deksa99 <deja99@live.com>
2022-08-18 18:24:47 +0200

    appointment service: model

--- AppointmentService/model/model.go
+++ AppointmentService/model/model.go
@@ -1,1 +1,60 @@
 package model
+
+import (
+	"database/sql/driver"
+	"gorm.io/gorm"
+	"time"
+)
+
+type RequestStatus string
+
+func (m *RequestStatus) Scan(value interface{}) error {
+	*m = RequestStatus(value.([]byte))
+	return nil
+}
+
+func (m RequestStatus) Value() (driver.Value, error) {
+	return string(m), nil
+}
+
+type AppointmentStatus string
+
+func (m *AppointmentStatus) Scan(value interface{}) error {
+	*m = AppointmentStatus(value.([]byte))
+	return nil
+}
+
+func (m AppointmentStatus) Value() (driver.Value, error) {
+	return string(m), nil
+}
+
+const (
+	Submitted RequestStatus = "Submitted"
+	Accepted                = "Accepted"
+	Rejected                = "Rejected"
+)
+
+const (
+	Scheduled AppointmentStatus = "Scheduled"
+	Cancelled                   = "Cancelled"
+	Finished                    = "Finished"
+)
+
+type AppointmentRequest struct {
+	gorm.Model
+	UserID    uint          `gorm:"not null;default:null"`
+	VehicleID uint          `gorm:"not null;default:null"`
+	ServiceID uint          `gorm:"not null;default:null"`
+	Status    RequestStatus `sql:"type:request_status"`
+}
+
+type Appointment struct {
+	gorm.Model
+	UserID     uint              `gorm:"not null;default:null"`
+	VehicleID  uint              `gorm:"not null;default:null"`
+	ServiceID  uint              `gorm:"not null;default:null"`
+	WorkerID   uint              `gorm:"not null;default:null"`
+	StartTime  time.Time         `gorm:"not null;default:null"`
+	FinishTime time.Time         `gorm:"default:null"`
+	Status     AppointmentStatus `sql:"type:appointment_status"`
+}
--- VehicleService/model/model.go
+++ VehicleService/model/model.go
@@ -1,8 +1,20 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"database/sql/driver"
+	"gorm.io/gorm"
+)
 
 type Manufacturer string
+
+func (m *Manufacturer) Scan(value interface{}) error {
+	*m = Manufacturer(value.([]byte))
+	return nil
+}
+
+func (m Manufacturer) Value() (driver.Value, error) {
+	return string(m), nil
+}
 
 const (
 	Toyota       Manufacturer = "Toyota"
@@ -19,7 +31,7 @@ const (
 type Vehicle struct {
 	gorm.Model
 	UserID        uint         `gorm:"not null;default:null"`
-	Manufacturer  Manufacturer `gorm:"not null;default:null"`
+	Manufacturer  Manufacturer `sql:"type:manufacturer"`
 	CarModel      string       `gorm:"default:null;size:256"`
 	Color         string       `gorm:"default:null;size:256"`
 	LicencePlate  string       `gorm:"not null;default:null;unique"`
