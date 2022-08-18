efbcde606e39b4fb8341f7b623f681b7670ef987
deksa99 <deja99@live.com>
2022-08-18 19:26:39 +0200

    appointment service: router

--- AppointmentService/handler/handler.go
+++ AppointmentService/handler/handler.go
@@ -1,1 +1,31 @@
 package handler
+
+import "net/http"
+
+func CreateAppointmentRequest(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CancelRequest(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetRequestsForUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetAppointmentsForUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateAppointment(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func ShowNewRequestsForService(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func ShowAppointmentsForWorker(w http.ResponseWriter, r *http.Request) {
+
+}
--- AppointmentService/router/router.go
+++ AppointmentService/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"AppointmentService/handler"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -8,6 +9,14 @@ import (
 
 func HandleRequests() {
 	router := mux.NewRouter().PathPrefix("/api/appointments").Subrouter()
+
+	router.HandleFunc("/requests", handler.CreateAppointmentRequest).Methods("POST")
+	router.HandleFunc("/requests/{userId}/cancel", handler.CancelRequest).Methods("PATCH")
+	router.HandleFunc("/requests/{userId}", handler.GetRequestsForUser).Methods("GET")
+	router.HandleFunc("/{userId}", handler.GetAppointmentsForUser).Methods("GET")
+	router.HandleFunc("/requests/{requestId}/accept", handler.CreateAppointment).Methods("PATCH")
+	router.HandleFunc("/requests/service/{serviceId}", handler.ShowNewRequestsForService).Methods("POST")
+	router.HandleFunc("/worker/{workerId}", handler.ShowAppointmentsForWorker).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8084", router))
 }
