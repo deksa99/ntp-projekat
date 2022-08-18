57f03d9e22a9678f1be1dfdc32bc8b2f56788e64
deksa99 <deja99@live.com>
2022-08-18 10:58:55 +0200

    car service service: routes

--- CarServiceService/handler/handler.go
+++ CarServiceService/handler/handler.go
@@ -1,1 +1,31 @@
 package handler
+
+import "net/http"
+
+func GetAllCarServices(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetCatalog(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func FindNearest(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateService(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateService(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func ChangeAvailability(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateCarService(w http.ResponseWriter, r *http.Request) {
+
+}
--- CarServiceService/router/router.go
+++ CarServiceService/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"CarServiceService/handler"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -8,6 +9,14 @@ import (
 
 func HandleRequests() {
 	router := mux.NewRouter().PathPrefix("/api/car-services").Subrouter()
+
+	router.HandleFunc("", handler.GetAllCarServices).Methods("GET")
+	router.HandleFunc("", handler.CreateCarService).Methods("POST")
+	router.HandleFunc("/near-me", handler.FindNearest).Methods("POST")
+	router.HandleFunc("/service", handler.CreateService).Methods("POST")
+	router.HandleFunc("/service", handler.UpdateService).Methods("PATCH")
+	router.HandleFunc("/service/{id}/change-availability", handler.ChangeAvailability).Methods("PATCH")
+	router.HandleFunc("/{id}/catalog", handler.GetCatalog).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8083", router))
 }
