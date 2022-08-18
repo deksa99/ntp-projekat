0f8f1c1ee5daf1030254f20f1bba2b67360271a0
deksa99 <deja99@live.com>
2022-08-18 10:52:13 +0200

    car service service: populate database

--- CarServiceService/model/model.go
+++ CarServiceService/model/model.go
@@ -1,1 +1,28 @@
 package model
+
+import "gorm.io/gorm"
+
+type Location struct {
+	gorm.Model
+	Latitude  float64 `gorm:"not null;default:null"`
+	Longitude float64 `gorm:"not null;default:null"`
+	Name      string  `gorm:"default:null;size:256"`
+}
+
+type CarService struct {
+	gorm.Model
+	Name       string `gorm:"not null;default:null;size:256"`
+	LocationID uint
+	Location   Location `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+}
+
+type Service struct {
+	gorm.Model
+	Name         string  `gorm:"not null;default:null;size:256"`
+	Description  string  `gorm:"not null;default:null;size:1028"`
+	Price        float32 `gorm:"not null;default:null"`
+	ExpectedTime uint    `gorm:"default:null"`
+	Available    bool    `gorm:"not null;default true"`
+	CarServiceID uint
+	CarService   CarService `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+}
--- CarServiceService/util/database/dbDriver.go
+++ CarServiceService/util/database/dbDriver.go
@@ -1,10 +1,55 @@
 package database
 
 import (
+	"CarServiceService/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
 	"os"
+)
+
+var (
+	locations = []model.Location{
+		{Longitude: 19.8298413, Latitude: 45.2618498, Name: "Bulevar oslobođenja 21, Novi Sad"},
+		{Longitude: 19.8615368, Latitude: 45.2542202, Name: "Vladimira Nazora 2, Petrovaradin"},
+		{Longitude: 19.8480734, Latitude: 45.2421371, Name: "Veljka Petrovica 5, Novi Sad"},
+		{Longitude: 19.848706, Latitude: 45.2224511, Name: "Moše Pijade 17, Sremska Kamenica"},
+	}
+	carServices = []model.CarService{
+		{Name: "Kvačilo", LocationID: 1},
+		{Name: "Mile i sin", LocationID: 1},
+		{Name: "Auto Servis Roki", LocationID: 1},
+		{Name: "Auto Servis Roki 2", LocationID: 1},
+	}
+	services = []model.Service{
+		{Name: "Mali servis", Description: "Mali servis, velika briga. Prepustite je nama.", Price: 7300, ExpectedTime: 3, CarServiceID: 1},
+		{Name: "Veliki servis", Description: "Veliki servis Vašeg vozila", Price: 27600, ExpectedTime: 5, CarServiceID: 1},
+		{Name: "Poliranje farova", Description: "Da blista!", Price: 1800, ExpectedTime: 1, CarServiceID: 1},
+		{Name: "Zamena sijalica", Description: "LED i Xenon", Price: 850, CarServiceID: 1},
+		{Name: "Zamena akulumatora", Description: "Akumulator nije uključen u cenu!", Price: 1350, CarServiceID: 1},
+
+		{Name: "Mali servis", Description: "Mali servis vozila, cena može da varira.", Price: 8370, ExpectedTime: 2, CarServiceID: 2},
+		{Name: "Veliki servis", Description: "Veliki servis vozila, cena može da varira.", Price: 29010, ExpectedTime: 4, CarServiceID: 2},
+		{Name: "Mašinsko ispravljanje felni", Description: "Ko strela!", Price: 1970, ExpectedTime: 1, CarServiceID: 2},
+		{Name: "Zavarivanje felne", Description: "Bez šavova", Price: 1830, CarServiceID: 2},
+		{Name: "Skidanje točka", Description: "Za felne 15-17", Price: 320, CarServiceID: 2},
+		{Name: "Montaža gume", Description: "Za felne 15-17", Price: 620, CarServiceID: 2},
+		{Name: "Balansiranje", Description: "Za felne 15-17", Price: 880, CarServiceID: 2},
+
+		{Name: "Mali servis", Description: "Mali servis vozila, cena može da varira.", Price: 9700, ExpectedTime: 2, CarServiceID: 3},
+		{Name: "Veliki servis", Description: "Veliki servis vozila, cena može da varira.", Price: 35050, ExpectedTime: 4, CarServiceID: 3},
+		{Name: "Skidanje točka", Description: "Za felne 19-20", Price: 380, CarServiceID: 3},
+		{Name: "Montaža gume", Description: "Za felne 19-20", Price: 750, CarServiceID: 3},
+		{Name: "Balansiranje", Description: "Za felne 19-20", Price: 990, CarServiceID: 3},
+
+		{Name: "Mali servis", Description: "Mali servis vozila, cena može da varira.", Price: 9700, ExpectedTime: 2, CarServiceID: 4},
+		{Name: "Veliki servis", Description: "Veliki servis vozila, cena može da varira.", Price: 35050, ExpectedTime: 4, CarServiceID: 4},
+		{Name: "Kontrola kočnica", Description: "Kontrola kočnica po najsavremenijim standardima", Price: 1600, ExpectedTime: 1, CarServiceID: 4},
+		{Name: "Kontrola amortizera", Description: "Kontrola amortizera po najsavremenijim standardima", Price: 1600, CarServiceID: 4},
+		{Name: "Skidanje točka", Description: "Za felne 21-28", Price: 430, CarServiceID: 4},
+		{Name: "Montaža gume", Description: "Za felne 21-28", Price: 880, CarServiceID: 4},
+		{Name: "Balansiranje", Description: "Za felne 21-28", Price: 1430, CarServiceID: 4},
+	}
 )
 
 var Db *gorm.DB
@@ -20,25 +65,45 @@ func ConnectToDatabase() {
 		log.Println("Successfully connected to DB")
 	}
 
-	// TODO drop
-	//err = Db.Migrator().DropTable(&model.___{})
-	//if err != nil {
-	//	log.Panic(err.Error())
-	//}
-	//
-	//log.Println("Tables dropped successfully")
+	err = Db.Migrator().DropTable(&model.Location{})
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	err = Db.Migrator().DropTable(&model.CarService{})
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	err = Db.Migrator().DropTable(&model.Service{})
+	if err != nil {
+		log.Panic(err.Error())
+	}
 
-	// TODO migrate
-	//err = Db.AutoMigrate(&model.___{})
-	//if err != nil {
-	//	log.Panic(err.Error())
-	//}
-	//
-	//log.Println("Migration successful")
+	log.Println("Tables dropped successfully")
 
-	// TODO populate
-	//for _, account := range ___ {
-	//	Db.Create(&account)
-	//}
-	//log.Println("DB seeding successful")
+	err = Db.AutoMigrate(&model.Location{})
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	err = Db.AutoMigrate(&model.CarService{})
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	err = Db.AutoMigrate(&model.Service{})
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	log.Println("Migration successful")
+
+	for _, account := range locations {
+		Db.Create(&account)
+	}
+	for _, account := range carServices {
+		Db.Create(&account)
+	}
+	for _, account := range services {
+		Db.Create(&account)
+	}
+
+	log.Println("DB seeding successful")
 }
